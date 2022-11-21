a3718884ad43cf86f53178f21474f903b1b2d041
Joshua Kattapuram <70146035+joshuakatt@users.noreply.github.com>
2022-11-21 15:02:14 -0330

    test threads created

--- code/src/textScraper.py
+++ code/src/textScraper.py
@@ -3,6 +3,9 @@ import collections.abc
 from pptx import Presentation
 import glob
 import textrazor
+import logging
+import threading
+import time
 
 
 pptData = []
@@ -42,3 +45,30 @@ for x in range(len(pptData)):
                 print(x, entity.wikipedia_link)
     except:
         pass
+
+
+
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    threads = list()
+    for index in range(3):
+        logging.info("Main    : create and start thread %d.", index)
+        x = threading.Thread(target=thread_function, args=(index,))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        logging.info("Main    : before joining thread %d.", index)
+        thread.join()
+        logging.info("Main    : thread %d done", index)
